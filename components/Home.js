370cb63f126eaeff2a26b993c28f16b31f610b7b
Mathieu <math.legargasson@gmail.com>
2024-06-04 21:01:30 +0200

    vercel en cours

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
 
 
   useEffect(()=> {
-    fetch('https://my-moviz-backend-xi-rosy.vercel.app/movies') // http://localhost:3000/movies
+    fetch('https://my-moviz-backend-orcin.vercel.app/movies') // http://localhost:3000/movies
       .then(response => response.json())
       .then(data => {
         setMoviesData(data.movies)
