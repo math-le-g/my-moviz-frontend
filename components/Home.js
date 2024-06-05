0d3b1f429cee27350d501de4f65d67fa45b73ae1
Mathieu <math.legargasson@gmail.com>
2024-06-05 19:24:56 +0200

    second commit

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
 
 
   useEffect(()=> {
-    fetch('http://localhost:3000/movies') // http://localhost:3000/movies
+    fetch('http://localhost:3000/movies') // http://localhost:3000/movies c'est le back ici
       .then(response => response.json())
       .then(data => {
         setMoviesData(data.movies)
